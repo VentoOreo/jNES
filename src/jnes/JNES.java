44606253501d546c548e24a8f35f32d3f3a778ab
Sergio <Sergio@Swagtop>
2017-04-14 22:49:33 -0500

    Changed main class slightly; automatically disassembles rather than based on command line argument

--- src/jnes/JNES.java
+++ src/jnes/JNES.java
@@ -3,9 +3,7 @@ package jnes;
 public class JNES {
 
     public static void main(String[] args) {
-        // Testing if disassemble arg is being used, and a path argument exists
-        if(args.length >= 2 && args[0].equals("-dis")){
-            Disassembler.disassemble(args[1]);
-        }
+        // Disassemble the ROM specified in the command line args
+        Disassembler.disassemble(args[0]);
     }   
 }
